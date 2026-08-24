06d7e521aced1679527e12e4cad48aa82c71e6c6
Rakhi Hari <rakhig20aaa@gmail.com>
2026-08-24 13:13:32 +0530

    tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Great_Vibes } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const greatVibes = Great_Vibes({
@@ -22,6 +23,36 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${greatVibes.variable} h-full antialiased`}
     >
+      {/* Meta Pixel Code */}
+      <Script
+        id="meta-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1599581298534068');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+      <noscript>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=1599581298534068&ev=PageView&noscript=1"
+          alt=""
+        />
+      </noscript>
+      {/* End Meta Pixel Code */}
       <body className="min-h-full flex flex-col site-shell">
         {/* Header */}
         <header className="topbar">
